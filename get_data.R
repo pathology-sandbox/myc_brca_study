d90cc8896a93c6efc125f277d1ff7c630253b290
gcampuzano <germancz81@gmail.com>
2018-04-14 16:48:33 -0500

    [get_data] Change directory for data

--- get_data.R
+++ get_data.R
@@ -2,7 +2,7 @@ library(xlsx)
 
 colclass <- rep('character', times=4)
 
-muts_amps <- read.xlsx("TCGA-and-HPA_DATA-MYC-BRCA1-BRCA2.xlsx", 
+muts_amps <- read.xlsx("data/TCGA-and-HPA_DATA-MYC-BRCA1-BRCA2.xlsx", 
                        sheetName = 'TCGA samples_mutations',
                        colClasses=colclass)
 
@@ -12,24 +12,24 @@ colclass <- c(
   rep('numeric', times=), 
   rep('character', times=2))
 
-brca1 <- read.xlsx("TCGA-and-HPA_DATA-MYC-BRCA1-BRCA2.xlsx", 
+brca1 <- read.xlsx("data/TCGA-and-HPA_DATA-MYC-BRCA1-BRCA2.xlsx", 
                    sheetName = 'BRCA1',
                    colClasses=colclass)
 
-brca2 <- read.xlsx("TCGA-and-HPA_DATA-MYC-BRCA1-BRCA2.xlsx", 
+brca2 <- read.xlsx("data/TCGA-and-HPA_DATA-MYC-BRCA1-BRCA2.xlsx", 
                    sheetName = 'BRCA1',
                    colClasses=colclass)
 
 colclass <- c('character', 'character', 'numeric')
 
-hpa_myc <- read.xlsx("TCGA-and-HPA_DATA-MYC-BRCA1-BRCA2.xlsx", 
+hpa_myc <- read.xlsx("data/TCGA-and-HPA_DATA-MYC-BRCA1-BRCA2.xlsx", 
                      sheetName = 'HPA_MYC',
                      colClasses=colclass)
 
-hpa_brca1 <- read.xlsx("TCGA-and-HPA_DATA-MYC-BRCA1-BRCA2.xlsx", 
+hpa_brca1 <- read.xlsx("data/TCGA-and-HPA_DATA-MYC-BRCA1-BRCA2.xlsx", 
                        sheetName = 'HPA_BRCA1', 
                        colClasses=colclass)
 
-hpa_brca2 <- read.xlsx("TCGA-and-HPA_DATA-MYC-BRCA1-BRCA2.xlsx", 
+hpa_brca2 <- read.xlsx("data/TCGA-and-HPA_DATA-MYC-BRCA1-BRCA2.xlsx", 
                        sheetName = 'HPA_BRCA2',
                        colClasses=colclass)
