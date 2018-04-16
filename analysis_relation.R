5d5689a1699a3a1d34e29134ee59f7a853ff62d7
gcampuzano <germancz81@gmail.com>
2018-04-15 22:44:55 -0500

    [analysis_relation] Unpack df_list dataframes from get_data.R module

--- analysis_relation.R
+++ analysis_relation.R
@@ -2,4 +2,12 @@
 
 source('get_data.R') # get tidy data via script
 # Variables: 
+mut_vs_amp <- df_list[[1]]
+muts_brca1 <- df_list[[2]]
+muts_brca2 <- df_list[[3]]
+demog <- df_list[[4]]
+amp_my <- df_list[[5]]
+amp_brca1 <- df_list[[6]]
+amp_brca2 <- df_list[[7]]
+rm("df_list")
 
