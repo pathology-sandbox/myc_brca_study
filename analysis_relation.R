8b22481e095955ee52fd655bffbaafd3b4f9d47a
gcampuzano <germancz81@gmail.com>
2018-04-21 10:03:40 -0500

    [analysis_relation] Exclude brca hpa tables and join demog table

--- analysis_relation.R
+++ analysis_relation.R
@@ -1,3 +1,5 @@
+library(psych)
+library(dplyr)
 
 
 source('get_data.R') # get tidy data via script
@@ -6,8 +8,7 @@ mut_vs_amp <- df_list[[1]]
 muts_brca1 <- df_list[[2]]
 muts_brca2 <- df_list[[3]]
 demog <- df_list[[4]]
-amp_my <- df_list[[5]]
-amp_brca1 <- df_list[[6]]
-amp_brca2 <- df_list[[7]]
+
 rm("df_list")
 
+df <- left_join(mut_vs_amp, demog, by = 'sample_id')
