9c5e2f8f34727d555fa5aa8803938dbfbc110661
gcampuzano <germancz81@gmail.com>
2018-04-21 12:18:57 -0500

    [analysis_relation] Fix text on Get Data section

--- analysis_relation.R
+++ analysis_relation.R
@@ -1,15 +1,13 @@
 library(psych)
 library(dplyr)
 
-
+# GET DATA 
 source('get_data.R') # get tidy data via script
-# Variables: 
 mut_vs_amp <- df_list[[1]]
 muts_brca1 <- df_list[[2]]
 muts_brca2 <- df_list[[3]]
 demog <- df_list[[4]]
-
-rm("df_list")
+rm("df_list") # Remove df_list obtained from source('get_data.R')
 
 df <- left_join(mut_vs_amp, demog, by = 'sample_id')
 df$age_dicot <- cut(df$age, breaks = c(-Inf, 60, +Inf), labels = c('<=60','>60'))
@@ -18,3 +16,6 @@ df$brca1_cat <- as.factor(df$brca1_cat)
 df$brca2_cat <- as.factor(df$brca2_cat)
 df$sex <- NULL
 df$race <- NULL
+
+
+# DESCRIPTIVE STATS
